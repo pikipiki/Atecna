411a49b7293f844dea9cd64602bdda801cbce1e0
pikipiki <franky.kp@gmail.com>
2017-09-25 03:15:04 +0200

    integration

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,14 @@ function plusDivs(n) {
 function showDivs(n) {
   let i;
   let x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {
+    slideIndex = 1;
+  }    
+  if (n < 1) {
+    slideIndex = x.length;
+  }
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block";  
 }
